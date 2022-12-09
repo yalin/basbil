38600d2712b7eeeaaf6c4eb7d395a8ef8bcee291
Yalin Aydin <myaydin@mun.ca>
2022-12-09 01:45:58 -0500

    Auto-generated commit [ci skip]

--- src/app/match-input/match-input.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-match-input',
-  templateUrl: './match-input.component.html',
-  styleUrls: ['./match-input.component.css']
-})
-export class MatchInputComponent {}
